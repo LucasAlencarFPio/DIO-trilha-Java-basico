cc711cd3a237bf3cbfc7fcb42e67afa65e863b6a
LucasAlencarFPio <lucasalencar1803@gmail.com>
2024-11-25 10:27:01 -0300

    Operadores

---
+++ estudo/src/edu/lucas/primeirasemana/Operadores.java
@@ -0,0 +1,9 @@
+package edu.lucas.primeirasemana;
+
+public class Operadores {
+    public static void main(String[] args){
+        //Operadores
+
+
+    }
+}
--- estudo/src/edu/lucas/primeirasemana/Tipos_Variaveis.java
+++ estudo/src/edu/lucas/primeirasemana/Tipos_Variaveis.java
@@ -1,8 +1,38 @@
 package edu.lucas.primeirasemana;
 
-
 public class Tipos_Variaveis {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+
+        //tipos de dados primitivos
+        //tipo/ valor minimo / valor maximo / memoria 
+        /*byte A = -128;  / 127 / 1byte
+        short B = -32768;  / 32.767 / 2byte
+        int C = -2147483648;  / 2.147.483.647 / 4byte
+        long D = -922337203L...  / 9.223.372.036.854.775.807/ 8byte
+        
+        float E = -3,4028F + 38; // / 3,4028E + 38 / 4byte
+        double F = -1,7976F + 308 + 38; // / 1,7976E + 308 / 8byte 
+
+        float termina com F e long L
+
+        String I = "texto"
+
+        boolean G = true; / false / 1bit
+        char H = 'A'; / 2bytes / Armazena um único caractere/letra ou valores ASCII */
+
+        //para representar números, na grande maioria das vezes utilizaremos o tipo int para representar números inteiros
+        //e double para representar números fracionados.
+
+        //<Tipo> <nomeVariavel> <atribuicaoDeValorOpcional>
+
+        //Exemplos abaixo:
+
+        int idade; //Tipo "int", nome "idade", com nenhum valor atribuído. 
+        int anoFabricacao = 2021; //tipo "int", nome "anoFabricacao", com valor 2021.
+        double salarioMinimo = 2.500; //tipo "double", nome "salarioMinimo", valor 2.500.
+
+        //final antes do tipo faz com que o valor da variavel não possa ser mudado (costumasse escrever o nome de variaveis final em caixa alta)
+
+        
     }
 }
